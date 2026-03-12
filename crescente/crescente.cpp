e17736367b87492cf88490e9cdf138a338f58e35
Guilherme Borges de Gouveia <guilhermeborgesgv@gmail.com>
2026-03-12 00:49:40 -0300

    Add files via upload

---
+++ crescente/crescente.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int x, y;
+
+    cout << "Digite dois numeros: " << endl;
+    cin >> x >> y;
+
+    while (x != y) {
+        if (x < y) {
+            cout << "Crescente" << endl;
+        } else {
+            cout << "Decrescente" << endl;
+        }
+
+        cout << "Digite outros dois numeros: " << endl;
+        cin >> x >> y;
+    }
+
+    return 0;
+}
